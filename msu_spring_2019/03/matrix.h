8b2905f23b09649f276408d2f45f9a7f0584a868
Vasiliy Viskov <47928580+vacbansry@users.noreply.github.com>
2019-03-30 10:54:24 +0300

    Create matrix.h

---
+++ msu_spring_2019/03/matrix.h
@@ -0,0 +1,80 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+class Matrix {
+public:
+    unsigned rows;
+    unsigned cols;
+    int32_t **data;
+
+    Matrix (unsigned r, unsigned c) {
+        rows = r;
+        cols = c;
+        data = new int32_t*[rows];
+        for (unsigned i = 0; i < rows; i++)
+            data[i] = new int32_t[cols];
+    }
+
+    ~Matrix () {}
+
+    unsigned getRows() const {
+        return this->rows;
+    }
+
+    unsigned getColumns() const {
+        return this->cols;
+    }
+
+    class Row {
+    public:
+        int32_t *data;
+        unsigned cols;
+
+        Row (int32_t *row, int32_t c)
+            :data (row)
+            ,cols (c) {}
+
+
+        int32_t& operator [] (size_t j) {
+            if (j >= cols || j < 0)
+                throw std::out_of_range("");
+            return this->data[j];
+        }
+    };
+
+    Row operator [] (size_t i) const {
+        if (i >= this->rows || i < 0)
+            throw std::out_of_range("");
+        Row row (this->data[i], this->cols);
+        return row;
+    }
+
+    bool operator == (const Matrix& other) const {
+        if (this == &other)
+            return 1;
+        if (this->rows != other.rows || this->cols != other.cols)
+            return 0;
+        for (unsigned i = 0; i < this->rows; i++) {
+            for (unsigned j = 0; j < this->cols; j++) {
+                if (other.data[i][j] != this->data[i][j])
+                    return 0;
+            }
+        }
+        return 1;
+    }
+
+    bool operator != (const Matrix& other) const {
+        return !(*this == other);
+    }
+
+    const Matrix& operator *= (const size_t value) {
+        for (unsigned i = 0; i < this->rows; i++) {
+            for (unsigned j = 0; j < this->cols; j++) {
+                this->data[i][j] *= value;
+            }
+        }
+        return *this;
+    }
+};
+
+#endif // MATRIX_H_INCLUDED
