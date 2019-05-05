ec9ea202ebfee7f2f1405b4d7101a3c838f7dff2
Vasiliy Viskov <47928580+vacbansry@users.noreply.github.com>
2019-05-05 21:04:14 +0300

    done

--- msu_spring_2019/11/main.cpp
+++ msu_spring_2019/11/main.cpp
@@ -5,21 +5,22 @@
 #include <future>
 
 enum {
-    MAX_SIZE = 400
+    MAX_SIZE = 50000,
+    OLOLO = 50000000
 };
 
 struct fInfo {
     std::fstream file;
     std::string name;
-    unsigned len = 0;
+    uint64_t len = 0;
 };
 
 using fileInfo = struct fInfo;
 
-void Merge(uint64_t arr[], unsigned l, unsigned m, unsigned r) {
-    unsigned i, j, k;
-    unsigned n1 = m - l + 1;
-    unsigned n2 = r - m;
+void Merge(uint64_t arr[], uint64_t l, uint64_t m, uint64_t r) {
+    uint64_t i, j, k;
+    uint64_t n1 = m - l + 1;
+    uint64_t n2 = r - m;
 
     uint64_t L[n1], R[n2];
 
@@ -47,19 +48,19 @@ void Merge(uint64_t arr[], unsigned l, unsigned m, unsigned r) {
     }
 }
 
-void MergeSort(uint64_t arr[], unsigned l, unsigned r) {
+void MergeSort(uint64_t arr[], uint64_t l, uint64_t r) {
     if (l < r) {
-	unsigned m = l + (r - l) / 2;
+	uint64_t m = l + (r - l) / 2;
 	MergeSort(arr, l, m);
 	MergeSort(arr, m + 1, r);
 	Merge(arr, l, m, r);
     }
 }
 
-unsigned lenFile(void) {
-    std::ifstream box("input.txt");
-    unsigned tmp;
-    unsigned len = 0;
+uint64_t lenFile(void) {
+    std::fstream box("input.txt", std::ios_base::in);
+    uint64_t tmp;
+    uint64_t len = 0;
     while (!box.eof()) {
         box >> tmp;
         len++;
@@ -68,11 +69,13 @@ unsigned lenFile(void) {
     return len;
 }
 
-fileInfo* placeFiles(unsigned quant) {
-    std::ifstream fin("input.txt");
+fileInfo* placeFiles(uint64_t quant) {
+    std::cout << "START\n";
+    std::fstream fin("input.txt", std::ios_base::in);
+    std::cout << "WOW\n";
     fileInfo* files = new fileInfo[quant];
     uint64_t box[MAX_SIZE];
-    for (unsigned i = 0; i < quant; i++) {
+    for (uint64_t i = 0; i < quant; i++) {
         while (!fin.eof() && files[i].len < MAX_SIZE) {
             fin >> box[files[i].len];
             files[i].len++;
@@ -80,14 +83,14 @@ fileInfo* placeFiles(unsigned quant) {
         MergeSort(box, 0, files[i].len - 1);
         files[i].name = std::to_string(i) + ".txt";
         files[i].file.open(files[i].name, std::ios_base::out);
-        for (unsigned j = 0; j < files[i].len; j++)
+        for (uint64_t j = 0; j < files[i].len; j++)
             files[i].file << box[j] << ' ';
         files[i].file.close();
     }
     return files;
 }
 
-fileInfo& mergeFiles(fileInfo& left, fileInfo& right, unsigned side) {
+fileInfo& mergeFiles(fileInfo& left, fileInfo& right, uint64_t side) {
     std::string name = "tmp" + std::to_string(side) + ".txt";
     std::fstream box(name, std::ios_base::out);
     left.file.open(left.name, std::ios_base::in);
@@ -97,9 +100,9 @@ fileInfo& mergeFiles(fileInfo& left, fileInfo& right, unsigned side) {
     bool lFlag = true;
     uint64_t rBox;
     bool rFlag = true;
-    unsigned leftLen = left.len;
-    unsigned rightLen = right.len;
-    unsigned allLen = leftLen + rightLen;
+    uint64_t leftLen = left.len;
+    uint64_t rightLen = right.len;
+    uint64_t allLen = leftLen + rightLen;
 
     while (leftLen && rightLen) {
 	if (lFlag) {
@@ -159,9 +162,9 @@ fileInfo& mergeFiles(fileInfo& left, fileInfo& right, unsigned side) {
     return left;
 }
 
-fileInfo& concatenateFiles(fileInfo* files, unsigned l, unsigned r, const unsigned side) {
+fileInfo& concatenateFiles(fileInfo* files, uint64_t l, uint64_t r, const uint64_t side) {
     if (l < r) {
-	unsigned m = l + (r - l) / 2;
+	uint64_t m = l + (r - l) / 2;
 	fileInfo& left = concatenateFiles(files, l, m, side);
 	fileInfo& right = concatenateFiles(files, m + 1, r, side);
 	return mergeFiles(left, right, side);
@@ -169,8 +172,8 @@ fileInfo& concatenateFiles(fileInfo* files, unsigned l, unsigned r, const unsign
     return files[l];
 }
 
-void removeAll(fileInfo* files, unsigned quant) {
-    for (unsigned i = 1; i < quant; i++) {
+void removeAll(fileInfo* files, uint64_t quant) {
+    for (uint64_t i = 1; i < quant; i++) {
         remove(files[i].name.c_str());
     }
     remove("tmp0.txt");
@@ -179,15 +182,27 @@ void removeAll(fileInfo* files, unsigned quant) {
     delete(files);
 }
 
+void createHuge(void) {
+    std::ofstream file("input.txt");
+    for (uint64_t i = OLOLO; i > 0; i--) {
+        file << i;
+        if (i != 1)
+            file << ' ';
+        if (i % 100000 == 0)
+            std::cout << i << '\n';
+    }
+    file.close();
+}
 
 int main(void) {
-    unsigned len = lenFile();
-    unsigned quant = len / MAX_SIZE;
+    //createHuge();
+    uint64_t len = lenFile();
+    uint64_t quant = len / MAX_SIZE;
     if (len % MAX_SIZE)
         quant++;
     fileInfo* files = placeFiles(quant);
     if (quant > 1) {
-        unsigned m = quant / 2 - 1;
+        uint64_t m = quant / 2 - 1;
         std::thread left(concatenateFiles, files, 0, m, 1);
         std::thread right(concatenateFiles, files, m + 1, quant - 1, 2);
         left.join();
@@ -195,5 +210,6 @@ int main(void) {
         mergeFiles(files[0], files[m + 1], 0);
     }
     removeAll(files, quant);
+
     return 0;
 }
